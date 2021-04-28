929cd83122005c55fcb6088d948e79ceb42b63e2
anatashi <anatashi@student.21-school.ru>
2021-04-28 16:35:45 +0300

    added test_ft_list.cpp

---
+++ ft_list/test_ft_list.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Arclight Natashia on 4/28/21.
+//
+
+#include <iostream>
+#include "ft_list.hpp"
+
+/*
+ *  go from the beginning to the end of the container
+ */
+void goToBeginToEnd()
+{
+
+}
+
+
+void iteratorTest()
+{
+    goToBeginToEnd();
+}
+
+
+int main ()
+{
+    iteratorTest();
+}
+
+
+
