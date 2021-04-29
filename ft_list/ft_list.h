0c863faa66af89bb865a88cfe9f736b7bb4e9396
anatashi <anatashi@student.21-school.ru>
2021-04-29 19:26:10 +0300

    allocator attributes are defined

--- ft_list/ft_list.h
+++ ft_list/ft_list.h
@@ -7,12 +7,33 @@
 
 #include "stdafx.h"
 
-class ft_list {
+namespace ft
 
-    private:
+template<class T, class Alloc = std::allocator<T> >
+class list {
 
-    public:
-        ft_list();
+private:
+
+public:
+    typedef T value_type;
+    typedef Alloc allocator_type;
+
+    typedef typename allocator_type::reference              reference;
+    typedef typename allocator_type::const_reference        const_reference;
+    typedef typename allocator_type::pointer                pointer;
+    typedef typename allocator_type::const_pointer          const_pointer;
+
+    /*
+     * define iterators
+     */
+
+    using size_type = size_t;
+    using ref = reference;
+    using c_ref = const_reference;
+    using ptr = pointer;
+    using c_ptr = const_pointer;
+
+    ft_list();
 };
 
 
