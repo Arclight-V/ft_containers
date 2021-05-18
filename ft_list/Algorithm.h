5ff1d624cbafae685d1972ba3e7bc4b19c2dc0f7
Arclight-V <vladimtor@gmail.com>
2021-05-18 13:38:50 +0300

    algorithm.h restored

---
+++ ft_list/Algorithm.h
@@ -0,0 +1,52 @@
+//
+// Created by Arclight Natashia on 5/18/21.
+//
+
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+namespace ft {
+    template<class T>
+    void swap(T &a, T &b) {
+        T tmp;
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    template<class InputIterator1, class InputIterator2>
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+        while (first1 != last1) {
+            if (!(*first1 == *first2)) // or: if (!pred(*first1,*first2)), for version 2
+                return false;
+            ++first1;
+            ++first2;
+        }
+        return true;
+    }
+
+    template<class InputIterator1, class InputIterator2>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                                 InputIterator2 first2, InputIterator2 last2) {
+        while (first1 != last1) {
+            if (first2 == last2 || *first2 < *first1)
+                return false;
+            else if (*first1 < *first2)
+                return true;
+            ++first1;
+            ++first2;
+        }
+        return (first2 != last2);
+    }
+
+    template<class T>
+    struct less {
+    public:
+        bool
+        operator()(const T &lhs, const T &rhs) const {
+            return (lhs < rhs);
+        }
+    };
+}
+
+#endif //ALGORITHM_H
